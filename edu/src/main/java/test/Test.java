b1d37f8322165ca03ca8779f518c6e4327079bfb
duzhiguo <2356028345@qq.com>
2018-10-08 14:22:18 +0800

    提交文件

---
+++ edu/src/main/java/test/Test.java
@@ -0,0 +1,30 @@
+package test;
+
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        if (i>5);
+
+        int i = scan.nextInt();
+
+        System.out.println(“i的值大于5”)
+
+ else
+
+        System.out.printf(“i的值小于等于5”);}
+
+
+
+
+
+}
--- edu/src/test/java/Test/FanTest.java
+++ edu/src/test/java/Test/FanTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,63 @@ public class FanTest {
 
     @Test
     public void setColor() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getColor1() {
+    }
+
+    @Test
+    public void setColor1() {
+    }
+
+    @Test
+    public void getSpeed() {
+    }
+
+    @Test
+    public void setSpeed() {
+    }
+
+    @Test
+    public void getRedius() {
+    }
+
+    @Test
+    public void setRedius() {
+    }
+
+
+    @Test
+    public void getColor2() {
+    }
+
+    @Test
+    public void setColor2() {
+    }
+
+    @Test
+    public void getSpeed1() {
+    }
+
+    @Test
+    public void setSpeed1() {
+    }
+
+    @Test
+    public void getRedius1() {
+    }
+
+    @Test
+    public void setRedius1() {
+    }
+
+    @Test
+    public String  toString() {
+   return null;
     }
 }
